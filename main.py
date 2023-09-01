95e320180c67999acc2d353bddc7794b014a7882
thefoodiee <67383814+thefoodiee@users.noreply.github.com>
2023-09-01 13:50:11 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur to reduce noise
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Threshold the image to highlight potential pallor
+    _, thresholded = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
+
+    # Find contours in the thresholded image
+    contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw rectangles around potential pallor areas
+    for contour in contours:
+        if cv2.contourArea(contour) > 200:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # Display the frame
+    cv2.imshow("Anemia Detection", frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
